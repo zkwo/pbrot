6f73f0bec452e1882c5ffe048290bf823dcfa09b
Jein Ananda <112226147+jeeexcalibur@users.noreply.github.com>
2026-01-05 14:52:20 +0800

    refactor: add analytics tool

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { inject } from '@vercel/analytics'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+
+// Initialize Vercel Analytics
+inject()
 </script>
 
 <template>
