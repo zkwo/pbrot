06664bc06cbe517884de4ac63551de4e4c2214b7
zkwo <ko40c980ew632@gmail.com>
2026-05-05 09:26:50 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,12 @@ import router from './router'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+
+// DARK MODE INIT
+if (
+  localStorage.getItem('theme') === 'dark' ||
+  (!localStorage.getItem('theme') &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark')
+}
